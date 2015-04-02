e50dcfdf62c5238df1ec12114481f368a59328ef
Matt Dainty <matt@bodgit-n-scarper.com>
2015-04-02 18:14:25 +0100

    Add basic tmux and vim config as a start

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Work out the directory containing this script
+dir=$(cd $(dirname ${0}); pwd -P)
+
+# If that directory is underneath $HOME then use relative symlinks
+if [ "${dir#${HOME}}" != "${dir}" ]; then
+  base=${dir#${HOME%/}/}
+else
+  base=${dir}
+fi
+
+ln -sfv ${base}/tmux.conf ~/.tmux.conf
+ln -sfv ${base}/vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,7 @@
+syn on
+set ruler
+set digraph
+set list
+set lcs=tab:»·,trail:·,eol:¶
+set modeline
+set modelines=5
